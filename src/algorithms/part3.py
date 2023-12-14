1bd098fe157c4eba1025c6e5eb857c0321b1acaa
PitriFerra <pitriferra@gmail.com>
2023-12-14 14:09:21 +0100

    update

--- src/algorithms/part3.py
+++ src/algorithms/part3.py
@@ -1,34 +1,6 @@
-def build_profiles(u, item, n, m, features):
-    profile = [n][[0.0] * features]
-    count = [n][[0.0] * features]  
-    avg = [n]
-
-    # compute the average rating for each driver
-    for i in range(n):
-        cnt = 0
-        for j in range(m):
-            if u[i][j] is not None:
-                cnt += 1
-                avg[i] += u[i][j]
-        if cnt != 0:
-            avg[i] /= cnt
-    
-    for i in range(n):  
-        for j in range(m):  
-            entry = u[i][j]  
-            if entry is not None:  
-                for f in range(features):  
-                    feature_weight = item[j][f]
-                    if feature_weight != 0.0:   
-                        count[i][f] += feature_weight 
-                        profile[i][f] += entry * feature_weight 
-
-    for i in range(n): 
-        for f in range(features):  
-            if count[i][f] != 0: 
-                profile[i][f] = (profile[i][f] - avg[i] * count[i][f]) / count[i][f] 
-                      
-    return profile
+import build_profiles
+import build_utility_matrix
+import json
 
 def edit_merch(trip_merch, keys, feature):
     if(keys[2] in trip_merch):
@@ -36,7 +8,14 @@ def edit_merch(trip_merch, keys, feature):
     else:
         trip_merch[keys[2]] = feature[1]
 
-profiles = build_profiles(u, item, n, m, features)
+
+with open('standard.json', 'r', encoding='utf-8') as f:
+    standard_data = json.load(f)
+
+with open('actual.json', 'r', encoding='utf-8') as f:
+    actual_data = json.load(f)
+
+profiles = build_profiles(build_utility_matrix(standard_data, actual_data), item, n, m, features)
 routes_part_3 = []
 
 for profile in profiles:
