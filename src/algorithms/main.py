27be268bdab8607d02b7e4480cf0236a1246d62c
EdomndDantes21 <redi.vreto@studenti.unitn.it>
2023-12-17 17:09:49 +0100

    minor fix

--- src/algorithms/main.py
+++ src/algorithms/main.py
@@ -1,4 +1,4 @@
-import build_profiles
+from build_profiles import build_profiles
 from  build_utility_matrix import build_utility_matrix
 from cluster import kmeans_cluster
 from build_route_vector import route_to_vector
@@ -7,7 +7,6 @@ import json
 from part3 import get_best_route
 
 def main():
-    print("hello :D")
     ###### PART1 #####
     # read all JSON routes and transfrom them into feature vectors
     with open('standard.json', 'r', encoding='utf-8') as f:
@@ -16,19 +15,23 @@ def main():
     with open('actual.json', 'r', encoding='utf-8') as f:
         actual_data = json.load(f)
         
-    drivers = set()
+    std_routes = [route_to_vector(item["route"]) for item in standard_data]
+    act_routes = [route_to_vector(item["route"]) for item in actual_data]
+    print(std_routes)
+    
         
+    drivers = set()
     for route in actual_data:
         drivers.add(route["driver"])
 
     n = len(drivers)
-    m = len(actual_data)
-        
+    m = len(act_routes)
+    
     # build utility matrix
     u = build_utility_matrix(standard_data, actual_data, drivers)
-    
+        
     # build user profiles
-    profiles = build_profiles(u, actual_data, n, m, len(profiles[0]))
+    profiles = build_profiles(u, act_routes, n, m, len(std_routes[0]))
 
     # cluster users
     clusters = kmeans_cluster(profiles, profiles.len())
