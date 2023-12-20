9357120d527ce844cbba077d70a27e6bfcd564dd
EdomndDantes21 <redi.vreto@studenti.unitn.it>
2023-12-20 22:33:24 +0100

    added state-of-the-art elbow finding algorithm

--- src/algorithms/cluster.py
+++ src/algorithms/cluster.py
@@ -1,12 +1,14 @@
 import numpy as np
 import unittest
 import random
-from sklearn.cluster import KMeans, DBSCAN, AgglomerativeClustering
+from sklearn.cluster import KMeans, DBSCAN
 from sklearn import preprocessing
 from dimensionality_reduction import svd
 from kneed import KneeLocator
 from sklearn.neighbors import NearestNeighbors
 from matplotlib import pyplot as plt
+from sklearn.datasets import make_blobs
+from sklearn.preprocessing import StandardScaler
 
 
 def kmeans_cluster(profile, n, reduce_dimensions, plot):
@@ -35,36 +37,31 @@ def kmeans_cluster(profile, n, reduce_dimensions, plot):
         The centroids
     '''
     if reduce_dimensions: 
-        dimensions = len(profile[0])
-        # dimensionality reduction
-        profile, model = svd(profile, dimensions, 0.1)
+        profile, model = svd(profile, len(profile[0]), 0.1)
         
     # normalize input so that the cosine distance is proportional to the square of the euclidian distance
     profile, norms = preprocessing.normalize(X=profile, axis=0, return_norm=True)
-        
-    k =  2                                    
-    prev_inertia = None
-    curr_inertia = None
-    delta_inertia = 1.0
-    kmeans = None
-    if plot:
-        err = []
-        ks = []
     
-    while (delta_inertia > 0.1 and k < len(profile) / 2): 
+    err = []
+    ks = []
+    k = 2
+    step = int(len(profile) / 50) + 1
+    
+    while k < len(profile):
+        print("k = ", k)
         kmeans = KMeans(n_clusters = k, n_init="auto").fit(profile)
-        if plot:
-            err.append(kmeans.inertia_)
-            ks.append(k)
+        err.append(kmeans.inertia_)
+        ks.append(k)
+        k += step
         
-        k *= 2
-        prev_inertia = curr_inertia
-        curr_inertia = kmeans.inertia_
-        if prev_inertia is not None:
-            delta_inertia = (prev_inertia - curr_inertia) / prev_inertia
-    
+    kneedle = KneeLocator(err, ks, S = 1.0, curve="convex", direction="decreasing")
     if plot:
-        plt.plot(ks, err)
+        print("elbow = ", kneedle.elbow_y)
+        
+        plt.plot(err, ks)
+        plt.title("Sum of the quadratic distance from the closest centroid of all points in the kmeans algorithm")
+        plt.ylabel("k")
+        plt.xlabel("Distance")
         plt.show()
     
     if reduce_dimensions:                
@@ -97,7 +94,6 @@ def kmeans_cluster(profile, n, reduce_dimensions, plot):
     - The output consists of labelled points, this is not optimal for the scope of this project
 '''
 def DBSCAN_cluster(profile, reduce_dimensions, plot):   
-    profile = None
     model = None
     
     # dimensionality reduction
@@ -107,18 +103,20 @@ def DBSCAN_cluster(profile, reduce_dimensions, plot):
         profile, model = svd(profile, dimensions, 0.1)
         
     # min_samples = 2 * #dimensions according to a popular heuristic in the Sander et. al paper (1998)
-    min_samples = min(2 * len(profile[0]), len(profile))
+    min_samples = 2 * len(profile[0])
+    if min_samples > len(profile):
+        min_samples = int(len(profile) / 2)
     
     #knn to find elbow point
-    neighbors = NearestNeighbors(n_neighbors=min_samples)
+    neighbors = NearestNeighbors(n_neighbors=min_samples, metric="cosine")
     neighbors_fit = neighbors.fit(profile)
     distances, indices = neighbors_fit.kneighbors(profile)
     distances = np.sort(distances, axis=0)
     distances = distances[:,1]
+    
     index = [i for i in range(len(distances))]
     
     kneedle = KneeLocator(index, distances, S = 1.0, curve="convex", direction="increasing")
-    
     if plot:
         print("elbow = ", kneedle.elbow_y)
         plt.plot(distances)
@@ -127,7 +125,7 @@ def DBSCAN_cluster(profile, reduce_dimensions, plot):
         plt.ylabel("Distance")
         plt.show()
     
-    return DBSCAN(eps=kneedle.elbow_y, min_samples=2*len(profile[0]), metric="cosine").fit(profile)
+    return DBSCAN(eps=kneedle.elbow_y, min_samples=min_samples, n_jobs=-1, metric="cosine").fit(profile)
     
 '''
 Unit tests 
@@ -135,7 +133,7 @@ Unit tests
 class TestBuildProfiles(unittest.TestCase): 
     def test_kmeans_cluster(self):
         n = 500   
-        d = 10
+        d = 15
         density = 30
         S = np.random.rand(n, d)
         
@@ -143,15 +141,37 @@ class TestBuildProfiles(unittest.TestCase):
             for j in range(d):
                 if random.randint(1,100) > density:
                     S[i][j] = 0.0
-        kmeans_cluster(S, n, True, True)
+        kmeans_cluster(S, n, False, False)
     
     def testDBSCAN(self):
-        n = 200   
-        d = 15
-        density = 20
+        n = 1100
+        d = 20
         S = np.random.rand(n, d) 
         
-        DBSCAN_cluster(S, reduce_dimensions=True, plot=True)
+        dbscan = DBSCAN_cluster(S, reduce_dimensions=False, plot=True)
+        s = set()
         
+        for el in dbscan.core_sample_indices_:
+            s.add(el)
+        
+        print("#clusters = ", len(s))
+        print("#points = ", len(S)) 
+   
+        
+    def testDBSCAN_cluster_count(self):
+        centers = [[2, 2], [-2, -2], [2, -2]]
+        X, labels_true = make_blobs(
+            n_samples=1000, centers=centers, cluster_std=0.4, random_state=0
+        )
+
+        X = StandardScaler().fit_transform(X)
+        dbscan = DBSCAN_cluster(X, False, False)
+        
+        s = set()
+        
+        for el in dbscan.core_sample_indices_:
+            s.add(el)  
+        
+            
 if __name__ == '__main__':
     unittest.main()
