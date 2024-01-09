5891f710faacccf032c364b8928d93527db2a5d3
Riccardo <prt.riccardo.01@gmail.com>
2024-01-09 21:28:26 +0100

    parameters std and act routes

--- src/data/generate_driver_attributes.py
+++ src/data/generate_driver_attributes.py
@@ -1,11 +1,25 @@
 import json
 import random
 
-data = []
-locations = ["Rome", "Milan", "Verona", "Florence", "Naples", "Turin", "Bologna", "Palermo", "Genoa", "Bari", "Catania", "Venice", "Cagliari", "Syracuse", "Brescia", "Pisa", "Reggio Calabria", "Parma", "Modena"]
-items = ["milk", "pens", "butter", "honey", "tomatoes", "bread"]
+#### Path of json file ####
+path_locations = 'locations.json' #path of the file with the list of locations
+path_items = 'items.json' #path of the file with the list of items
+path_driver_attr = 'driver_attributes.json' #path of the output of driver_attributes json file 
 
-for i in range(100):
+#### Parameters ####
+n_locations = 20 #number of locations from the json file 
+n_items = 8 #number of items from the json file
+n_drivers = 100 #number of drivers
+
+with open(path_locations, 'r', encoding='utf-8') as locations_file:
+    locations = json.load(locations_file)[:n_locations]
+
+with open(path_items, 'r', encoding='utf-8') as items_file:
+   items = json.load(items_file)[:n_items] 
+
+data = []
+
+for i in range(n_drivers):
     upper_bound = random.randint(0, 10) / 10
     lower_bound = random.randint(0, 10) / 10
 
@@ -24,5 +38,5 @@ for i in range(100):
         }
     })
 
-with open('data/driver_attributes.json', 'w', encoding='utf-8') as f:
+with open(path_driver_attr, 'w', encoding='utf-8') as f:
     json.dump(data, f, ensure_ascii=False, indent=4)
