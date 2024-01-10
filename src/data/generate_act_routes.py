796feb712387180bf4e2808fb34c6614c52e622b
PitriFerra <pitriferra@gmail.com>
2024-01-10 14:32:26 +0100

    Huge update to data generation

--- src/data/generate_act_routes.py
+++ src/data/generate_act_routes.py
@@ -1,19 +1,4 @@
-import json
 import random
-from copy import deepcopy
-
-#### Path of json file ####
-path_items = 'data/items.json' #path of the file with the list of items
-path_driver_attr = 'data/driver_attributes.json' #path of the file with the driver_attributes
-path_standard = 'data/standard.json' #path of the the standard routes 
-path_actual = 'data/actual.json' #path of the output of the actual routes json file 
-
-#### Parameters ####
-n_items = 8 #number of items from the json file
-x = 0.15 #how much to fill the utility matrix
-
-with open(path_items, 'r', encoding='utf-8') as items_file:
-   items = json.load(items_file)[:n_items]
 
 def edit_merch(merchandise, pref_merch, stubborness):
     merch = merchandise.copy()
@@ -34,7 +19,7 @@ def edit_merch(merchandise, pref_merch, stubborness):
         if product not in merch and random.random() < stubborness["add_merch"] / (1 + pref_merch.index(product)):
             merchandise[product] = 30
 
-def generate_merchandise(pref_merch, stubborness):
+def generate_merchandise(pref_merch, stubborness, items):
     result = {}
     merchandise = random.sample(items, len(items))
     
@@ -44,16 +29,15 @@ def generate_merchandise(pref_merch, stubborness):
     edit_merch(result, pref_merch, stubborness)
     return result
 
-def modify_route(actual_element, driver):
+def modify_route(act_route, driver, items):
     stubborness = driver["stubborness"]
-    act_route = actual_element["route"]
     pref_cities = driver["preferences"]["cities"]
     pref_merch = driver["preferences"]["merchandise"]
     n_cities = len(pref_cities)
     cities = [act_route[0]["from"]]
 
     for trip in act_route:
-        if random.random() < stubborness["del_city"] / (n_cities - pref_cities.index(trip["to"])):
+        if len(act_route) > 1 and random.random() < stubborness["del_city"] / (n_cities - pref_cities.index(trip["to"])):
             i = act_route.index(trip)
             
             if i < len(act_route) - 1:
@@ -66,39 +50,16 @@ def modify_route(actual_element, driver):
     
     for city in pref_cities:
         if city not in cities and random.random() < stubborness["add_city"] / (1 + pref_cities.index(city)):
-            #print(f"Driver {driver['id']} added {city} with probability {stubborness['add_city'] / (1 + pref_cities.index(city))} in route {actual_element['sroute']}")
             route_length = len(act_route)
             pos = random.randint(0, route_length)
-            act_route.insert(pos, {"from": city if pos != route_length else act_route[-1]["to"], "to": city if pos == route_length else act_route[pos]["from"], "merchandise": generate_merchandise(pref_merch, stubborness) if pos == 0 or pos == route_length else act_route[pos - 1]["merchandise"]})
+            act_route.insert(pos, 
+                             {"from": city if pos != route_length else act_route[-1]["to"], 
+                              "to": city if pos == route_length else act_route[pos]["from"], 
+                              "merchandise": generate_merchandise(pref_merch, stubborness, items) if pos == 0 or pos == route_length else act_route[pos - 1]["merchandise"]})
             cities.append(city)
 
             if pos != 0 and pos != route_length:
                 act_route[pos - 1]["to"] = city
-                act_route[pos - 1]["merchandise"] = generate_merchandise(pref_merch, stubborness)
-
-# Load driver.json and standard.json
-with open(path_driver_attr, 'r', encoding='utf-8') as f:
-    driver_data = json.load(f)
-
-with open(path_standard, 'r', encoding='utf-8') as f:
-    standard_data = json.load(f)
-
-# Create actual.json data
-actual_data = []
-element_id = 1
-for driver in driver_data:
-    for standard in standard_data:
-        if random.random() < x:
-            actual_element = {
-                "id": f"a{element_id}",
-                "driver": driver["id"],
-                "sroute": standard["id"],
-                "route": deepcopy(standard["route"])
-            }
-            modify_route(actual_element, driver)
-            actual_data.append(actual_element)
-            element_id += 1
-
-# Write actual.json
-with open(path_actual, 'w', encoding='utf-8') as f:
-    json.dump(actual_data, f, ensure_ascii=False, indent=4)
+                act_route[pos - 1]["merchandise"] = generate_merchandise(pref_merch, stubborness, items)
+    
+    return act_route
--- src/evaluating_precision.py
+++ src/evaluating_precision.py
@@ -12,6 +12,8 @@ def part2():
     return
 
 def part3():
+    n_locations = 20 #number of locations from the json file 
+    n_items = 8 #number of items from the json file
     with open('solutions/part3.json', 'r', encoding='utf-8') as f:
         part3 = json.load(f)
     with open('data/driver_attributes.json', 'r', encoding='utf-8') as f:
@@ -20,18 +22,22 @@ def part3():
         actual_data = json.load(f)
     with open('data/standard.json', 'r', encoding='utf-8') as f:
         standard_data = json.load(f)
+    with open('data/locations.json', 'r', encoding='utf-8') as locations_file:
+        locations = json.load(locations_file)[:n_locations]
+    with open('data/items.json', 'r', encoding='utf-8') as items_file:
+        items = json.load(items_file)[:n_items] 
     j = 0
     sum_success_rate = 0
     global_sum_similarity = 0
+    features = def_features(standard_data, actual_data)
 
     while j < 100:
         similarity = []
-        features = def_features(standard_data, actual_data)
 
         for driver in drivers:
-            standard_route = generate_random_route()
+            standard_route = generate_random_route(items, locations)
             similarity.append([cosine_similarity([route_to_vector(standard_route, features)],
-                                                 [route_to_vector(modify_route(standard_route, driver), features)]).item()])
+                                                 [route_to_vector(modify_route(standard_route, driver, items), features)]).item()])
 
         i = 0
         successes = 0
@@ -39,7 +45,7 @@ def part3():
 
         for solution in part3:
             similarity[i].append(cosine_similarity([route_to_vector(solution["route"], features)],
-                                                   [route_to_vector(modify_route(solution["route"], drivers[i]), features)]).item())
+                                                   [route_to_vector(modify_route(solution["route"], drivers[i], items), features)]).item())
             sum_similarity += similarity[i][1]
 
             if similarity[i][1] > similarity[i][0]:
